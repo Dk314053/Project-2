171ed503cafc6c327fca6c69612dcbe92bac352b
Dk314053 <dk314053@gmail.com>
2023-04-28 15:08:31 +0530

    new changes day3 trend box

--- src/Component/Navbar Pages/section3/Column3.js
+++ src/Component/Navbar Pages/section3/Column3.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from "./column3.module.css"
 import Trend from './Trend BOX/Trend';
+import Follow from './Trend BOX/Follow';
 const Column3 = () => {
     return (
         <div className={styles.column3}>
            <Trend />
+           <Follow/>
         </div>
     );
 }
